055163acb05fd8348c9e0d6f3326b0cfb748a795
Rodrigo <Rodrigo@104.39.94.181>
2018-04-19 14:50:11 -0400

    Created lists

--- src/QuestionLists.java
+++ src/QuestionLists.java
@@ -3,7 +3,17 @@ import java.util.ArrayList;
 
 
 public class QuestionLists {
-    private ArrayList<QandA> questionlist;
+    private ArrayList<QandA> popCulture;
+    private ArrayList<QandA> technology; 
+    private ArrayList<QandA> nature; 
+    private ArrayList<QandA> music; 
+    private ArrayList<QandA> sports; 
+    private ArrayList<QandA> food; 
+    
+    public boolean addQuestion(String question, String answer)
+    {
+        return popCulture.add(new QandA(question, answer));
+    }
     
     public QuestionLists(){
         
