9237d3d74530ee8745c755bbf75cd3341bac3666
Rodrigo <Rodrigo@104.39.94.181>
2018-04-24 14:18:57 -0400

    Created animal questions

--- src/QandA.java
+++ src/QandA.java
@@ -14,4 +14,26 @@ public class QandA {
         }
         else return false;
     }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    @Override
+    public String toString() {
+        return "QandA{" + "question=" + question + ", answer=" + answer + '}';
+    }
+    
 }
--- src/QuestionLists.java
+++ src/QuestionLists.java
@@ -3,20 +3,27 @@ import java.util.ArrayList;
 
 
 public class QuestionLists {
-    private ArrayList<QandA> popCulture;
+    private ArrayList<QandA> animals;
     private ArrayList<QandA> technology; 
     private ArrayList<QandA> nature; 
-    private ArrayList<QandA> music; 
     private ArrayList<QandA> sports; 
     private ArrayList<QandA> food; 
     
-    public boolean addQuestion(String question, String answer)
-    {
-        return popCulture.add(new QandA(question, answer));
-    }
+//    public void addPopCulture(String question, String answer)
+//    {
+//        return popCulture.add(new QandA(question, answer));
+//    }
     
-    public QuestionLists(){
+    
+    public void QuestionLists(){
+        //pop culture list
+        QandA a1 = new QandA("Lemurs are native to only what Country?", "Madagascar"); animals.add(a1);
+        QandA a2 = new QandA("How many legs does a spider have?", "8"); animals.add(a2);
+        QandA a3 = new QandA("Canis lupus familaris is the scientific name for which common animal", "Dog"); animals.add(a3);
         
         
+        
+        //
     }
+
 }
